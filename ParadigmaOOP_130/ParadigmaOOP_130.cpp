f4814558c6b77cf85370154e0bc5a6a7807b0fa1
Kevinzain29 <ahmad.kevin.ft22@mail.umy.ac.id>
2023-05-22 13:29:35 +0700

    pelajar dibuat

--- ParadigmaOOP_130/ParadigmaOOP_130.cpp
+++ ParadigmaOOP_130/ParadigmaOOP_130.cpp
@@ -20,4 +20,19 @@ public:
 	}
 };
 
+class pelajar : public orang {
+public:
+	string sekolah;
+
+	pelajar(string pNama, string pSekolah) :
+		orang(pNama),
+		sekolah(pSekolah) {
+		cout << "pelajar dibuat\n" << endl;
+	}
+	~pelajar() {
+		cout << 
+	}
+};
+
+
 
